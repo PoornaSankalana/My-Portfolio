de4209636807d1ccffff8341ce5f41aef59b1ada
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2023-07-17 12:01:05 +0530

    Add links to socials

--- src/Components/Header/headerSocials.jsx
+++ src/Components/Header/headerSocials.jsx
@@ -5,13 +5,19 @@ import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header_social">
-      <a href="#" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/poorna-sankalana-67213620b/"
+        target="_blank"
+      >
         <BsLinkedin />
       </a>
-      <a href="#" target="_blank">
+      <a href="https://github.com/PoornaSankalana" target="_blank">
         <BsGithub />
       </a>
-      <a href="#" target="_blank">
+      <a
+        href="https://www.facebook.com/profile.php?id=100091867815707"
+        target="_blank"
+      >
         <BsFacebook />
       </a>
     </div>
